9a075e4d4f306ee67afa0e9b277e6c60abe2d0b0
Marzena Skórska <maczenta@gmail.com>
2024-06-08 08:34:00 +0000

    edit settings and env.py

--- catch_your_tea/settings.py
+++ catch_your_tea/settings.py
@@ -221,25 +221,14 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 #    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #   DEFAULT_FROM_EMAIL = 'catchyourtea@example.com'
 #  else:
-#   EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#
-#   EMAIL_USE_TLS = True
-#    EMAIL_PORT = 587
-#    EMAIL_HOST = 'smtp.gmail.com'
-#    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-#    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-#    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
-ACCOUNT_USERNAME_MIN_LENGTH = 4
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-WSGI_APPLICATION = 'vapeshop.wsgi.application'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # Default primary key field type
