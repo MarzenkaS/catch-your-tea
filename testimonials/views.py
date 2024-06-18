5a0ae95a66e51148ef7700868287149db0734ded
Marzena Skórska <maczenta@gmail.com>
2024-06-18 17:26:40 +0000

    add testimonial_list view

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Testimonial, ProductTestimonial
+from .forms import TestimonialForm
 
-# Create your views here.
+
+def testimonial_list(request):
+    testimonials = Testimonial.objects.all()
+    return render(request, 'testimonials/testimonial_list.html',
+                  {'testimonials': testimonials})
