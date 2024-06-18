f551c986dfb8b7abe89af9009b714d79a286ec32
Marzena Skórska <maczenta@gmail.com>
2024-06-18 19:27:04 +0000

    add edit and delete testimonial, edit urls.py

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.testimonial_list, name='testimonial_list'),
     path('add/', views.add_testimonial, name='add_testimonial'),
+    path('edit/<int:pk>/', views.edit_testimonial, name='edit_testimonial'),
+    path('delete/<int:pk>/', views.delete_testimonial,
+         name='delete_testimonial'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Testimonial, ProductTestimonial
 from .forms import TestimonialForm
 
@@ -18,3 +18,25 @@ def add_testimonial(request):
     else:
         form = TestimonialForm()
     return render(request, 'testimonials/add_testimonial.html', {'form': form})
+
+
+def edit_testimonial(request, pk):
+    testimonial = get_object_or_404(Testimonial, pk=pk)
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST, instance=testimonial)
+        if form.is_valid():
+            form.save()
+            return redirect('testimonial_list')
+    else:
+        form = TestimonialForm(instance=testimonial)
+    return render(request, 'testimonials/edit_testimonial.html',
+                  {'form': form})
+
+
+def delete_testimonial(request, pk):
+    testimonial = get_object_or_404(Testimonial, pk=pk)
+    if request.method == 'POST':
+        testimonial.delete()
+        return redirect('testimonial_list')
+    return render(request, 'testimonials/delete_testimonial.html',
+                  {'testimonial': testimonial})
