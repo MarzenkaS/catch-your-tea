5e98aca604f9ceeecbc9e046f9a6641a3da833e6
Marzena Skórska <maczenta@gmail.com>
2024-06-18 19:16:27 +0000

    add add_testimonial view and edit urls.py

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.testimonial_list, name='testimonial_list'),
+    path('add/', views.add_testimonial, name='add_testimonial'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Testimonial, ProductTestimonial
 from .forms import TestimonialForm
 
@@ -7,3 +7,14 @@ def testimonial_list(request):
     testimonials = Testimonial.objects.all()
     return render(request, 'testimonials/testimonial_list.html',
                   {'testimonials': testimonials})
+
+
+def add_testimonial(request):
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('testimonial_list')
+    else:
+        form = TestimonialForm()
+    return render(request, 'testimonials/add_testimonial.html', {'form': form})
