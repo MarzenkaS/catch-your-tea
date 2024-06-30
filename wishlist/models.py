cdcde36ddf1fe0a7e676fa4616923ed692396ea6
Marzena Skórska <maczenta@gmail.com>
2024-06-30 16:52:29 +0000

    add tea and wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tea(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teas = models.ManyToManyField(Tea)
+
+    def __str__(self):
+        return f"Wishlist of {self.user.username}"
