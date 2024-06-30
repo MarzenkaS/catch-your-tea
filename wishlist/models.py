fb778700cb757e28eac2e98e4f564d2d32c3ba35
Marzena Skórska <maczenta@gmail.com>
2024-06-30 20:38:42 +0000

    edit models for products and wishlist app, producta and wishlist templates

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products.models import Product
 
 
 class Tea(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    amount_in_gram = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Wishlist(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    teas = models.ManyToManyField(Tea)
+    products = models.ManyToManyField(Product)
 
     def __str__(self):
-        return f"Wishlist of {self.user.username}"
+        return f"Wishlist for {self.user.username}"
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
-from .models import Tea, Wishlist
+from .models import Wishlist
+from products.models import Product
 
 
-@login_required
 def wishlist_view(request):
-    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
-    teas_in_wishlist = wishlist.teas.all()
-    return render(request, 'wishlist/wishlist.html', {'teas': teas_in_wishlist})
+    wishlist = Wishlist.objects.get(user=request.user)
+    teas = wishlist.products.all()
+    context = {
+        'teas': teas,
+    }
+    return render(request, 'wishlist/wishlist.html', context)
 
 
 @login_required
-def add_to_wishlist(request, tea_id):
-    tea = get_object_or_404(Tea, id=tea_id)
+def add_to_wishlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
-    wishlist.teas.add(tea)
+    wishlist.products.add(product)
     return redirect('wishlist')
 
 
 @login_required
-def remove_from_wishlist(request, tea_id):
-    tea = get_object_or_404(Tea, id=tea_id)
+def remove_from_wishlist(request, product_id):
+    tea = get_object_or_404(Product, id=product_id)
     wishlist = get_object_or_404(Wishlist, user=request.user)
 
     if tea in wishlist.teas.all():
