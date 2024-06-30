5f9ad98f04a134ff80d35c02d3b01f87e7e2b54f
Marzena Skórska <maczenta@gmail.com>
2024-06-30 18:07:12 +0000

    edit urls.py for wishlist app

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('wishlist/', views.wishlist_view, name='wishlist'),
+    path('add-to-wishlist/<int:tea_id>/', views.add_to_wishlist,
+         name='add_to_wishlist'),
+    path('remove-from-wishlist/<int:tea_id>/', views.remove_from_wishlist,
+         name='remove_from_wishlist'),
 ]
