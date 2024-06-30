a8032365c21d247df675bb2e656f4cc5267adf63
Marzena Skórska <maczenta@gmail.com>
2024-06-30 18:02:54 +0000

    add remove_from_wishlist view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from .models import Tea, Wishlist
 
 
@@ -16,3 +17,15 @@ def add_to_wishlist(request, tea_id):
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
     wishlist.teas.add(tea)
     return redirect('wishlist')
+
+
+@login_required
+def remove_from_wishlist(request, tea_id):
+    tea = get_object_or_404(Tea, id=tea_id)
+    wishlist = get_object_or_404(Wishlist, user=request.user)
+
+    if tea in wishlist.teas.all():
+        wishlist.teas.remove(tea)
+        return redirect('wishlist')
+    else:
+        return HttpResponseForbidden("You do not have permission to remove this item from your wishlist.")
