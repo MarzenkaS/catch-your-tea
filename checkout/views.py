a38f84e3599be2b427e7416897c5aa2796d6b275
Marzena Skórska <maczenta@gmail.com>
2024-07-07 16:31:03 +0000

    edit checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from bag.models import BagItem
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
@@ -37,21 +36,7 @@ def checkout(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        # Synchronize the session bag with the database bag items for authenticated users
-        if request.user.is_authenticated:
-            bag = {}
-            bag_items_db = BagItem.objects.filter(user=request.user)
-            for item in bag_items_db:
-                if item.amount:
-                    if item.product.id in bag:
-                        bag[item.product.id]['items_by_amount'][item.amount] = item.quantity
-                    else:
-                        bag[item.product.id] = {'items_by_amount': {item.amount: item.quantity}}
-                else:
-                    bag[item.product.id] = item.quantity
-            request.session['bag'] = bag  # Ensure the session bag is updated
-        else:
-            bag = request.session.get('bag', {})
+        bag = request.session.get('bag', {})
 
         form_data = {
             'full_name': request.POST['full_name'],
@@ -90,31 +75,20 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
-                    messages.error(request, "One of the products in your bag wasn't found in our database. Please call us for assistance!")
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. Please double check your information.')
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
-        # Fetch bag data for authenticated users from the database
-        if request.user.is_authenticated:
-            bag_items_db = BagItem.objects.filter(user=request.user)
-            bag = {}
-            for item in bag_items_db:
-                if item.amount:
-                    if item.product.id in bag:
-                        bag[item.product.id]['items_by_amount'][item.amount] = item.quantity
-                    else:
-                        bag[item.product.id] = {'items_by_amount': {item.amount: item.quantity}}
-                else:
-                    bag[item.product.id] = item.quantity
-            request.session['bag'] = bag  # Ensure the session bag is updated
-        else:
-            bag = request.session.get('bag', {})
-
+        bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
@@ -148,18 +122,14 @@ def checkout(request):
             order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
-        'bag_items': current_bag['bag_items'],
-        'total': current_bag['total'],
-        'delivery': current_bag['delivery'],
-        'grand_total': current_bag['grand_total'],
-        'product_count': current_bag['product_count'],
     }
 
     return render(request, template, context)
@@ -181,7 +151,6 @@ def checkout_success(request, order_number):
         # Save the user's info
         if save_info:
             profile_data = {
-                'full_name': order.full_name,
                 'default_phone_number': order.phone_number,
                 'default_country': order.country,
                 'default_postcode': order.postcode,
@@ -203,7 +172,6 @@ def checkout_success(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'on_profile_page': True
     }
 
     return render(request, template, context)
