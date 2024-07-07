76cd2d285d08724a013f2e32156675e140471304
Marzena Skórska <maczenta@gmail.com>
2024-07-07 19:36:29 +0000

    edit webhooks.py and webhook_handler

--- profiles/views.py
+++ profiles/views.py
@@ -21,7 +21,7 @@ def profile(request):
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+    orders = profile.orders.all().order_by('-date')
 
     template = 'profiles/profile.html'
     context = {
@@ -33,6 +33,7 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
